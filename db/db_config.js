ba40dd5f6531378129fa2398808a5b18c7a515a0
MarcoF1 <marco1fleming@gmail.com>
2021-03-06 15:14:13 -0500

    added postgress, only tested on user CRUD, so hopefully it works for everything

--- db/db_config.js
+++ db/db_config.js
@@ -1,6 +1,15 @@
-const sqlite3 = require('sqlite3');
+const { Pool } = require("pg");
+// const sqlite3 = require('sqlite3');
 
-let sqlDb;
+production = false;
+
+// let sqlDb;
+// Connect to the db using a pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: production ? { rejectUnauthorized: false } : false,
+  database: "grassroots",
+});
 
 const columnNames = {
   user_id: "user_id",
@@ -12,7 +21,7 @@ const columnNames = {
   text: "text",
   tags: "tags",
   deadline: "deadline",
-  
+
   government_id: "government_id",
   name: "name",
   description: "description",
@@ -20,104 +29,136 @@ const columnNames = {
   address: "address",
 
   bill_id: "bill_id",
-  closing_date: "closing_date"
+  closing_date: "closing_date",
+  bill_status: "bill_status",
 
+  resource_id: "resource_id",
+  url: "url",
 };
 Object.freeze(columnNames);
 
-function createDb() {
+// function createDb() {
+//   console.log("created our db!");
+//   sqlDb = new sqlite3.Database("db.db", function() {
+//     createGovernmentsTable();
+//     createUserTable();
+
+//     createUserGovernmentsTable();
+
+//     createBillsTable();
+
+//     createResourcesTable();
+//   });
+// }
+async function createDb() {
+  // create user and shorts tables
+  await createGovernmentsTable();
+  await createUserTable();
+  await createUserGovernmentsTable();
+  await createBillsTable();
+  await createResourcesTable();
+
+  // await createUserTable();
+  // await createShortsTable();
+  // // create super user
+  // await createSuperUser();
   console.log("created our db!");
-  sqlDb = new sqlite3.Database('db.db', function() {
-    
-    createGovernmentsTable();
-    createUserTable();
+}
 
-    createUserGovernmentsTable();
-
-    createBillsTable();
-
-  });
-};
-
-
-function createGovernmentsTable() {
-  sqlDb.run(`CREATE TABLE IF NOT EXISTS governments (
-    ${columnNames.government_id} INTEGER PRIMARY KEY AUTOINCREMENT,
+async function createGovernmentsTable() {
+  await pool.query(`CREATE TABLE IF NOT EXISTS governments (
+    ${columnNames.government_id} SERIAL UNIQUE,
     ${columnNames.name} TEXT,
     ${columnNames.description} TEXT,
     ${columnNames.contact} TEXT,
     ${columnNames.address} TEXT
   )`);
-};
+}
 
-function createUserTable() {
-  sqlDb.run(`CREATE TABLE IF NOT EXISTS users (
-    ${columnNames.user_id} INTEGER PRIMARY KEY AUTOINCREMENT,
+async function createUserTable() {
+  await pool.query(`CREATE TABLE IF NOT EXISTS users (
+    ${columnNames.user_id} SERIAL UNIQUE,
     ${columnNames.username} TEXT NOT NULL UNIQUE,
     ${columnNames.password} TEXT NOT NULL
   )`);
-};
+}
 
-function createUserGovernmentsTable() {
-  sqlDb.run(`CREATE TABLE IF NOT EXISTS user_governments (
-    ${columnNames.user_id} INTEGER KEY,
-    ${columnNames.government_id} INTEGER KEY,
+async function createUserGovernmentsTable() {
+  await pool.query(`CREATE TABLE IF NOT EXISTS user_governments (
+    ${columnNames.user_id} INTEGER NOT NULL,
+    ${columnNames.government_id} INTEGER NOT NULL,
     ${columnNames.is_rep} BOOLEAN,
-    ${columnNames.description} TEXT,
-    FOREIGN KEY (${columnNames.user_id}) REFERENCES users(${columnNames.user_id})
-    FOREIGN KEY (${columnNames.government_id}) REFERENCES governments(${columnNames.government_id})
+    ${columnNames.description} TEXT NOT NULL,
+    FOREIGN KEY (${columnNames.user_id}) 
+    REFERENCES users(${columnNames.user_id}),
+    FOREIGN KEY (${columnNames.government_id}) 
+    REFERENCES governments(${columnNames.government_id})
   )`);
-};
+}
 
-function createBillsTable() {
-  sqlDb.run(`CREATE TABLE IF NOT EXISTS bills (
-    ${columnNames.bill_id} INTEGER PRIMARY KEY AUTOINCREMENT,
-    ${columnNames.government_id} INTEGER KEY,
-    ${columnNames.name} INTEGER KEY,
+async function createBillsTable() {
+  await pool.query(`CREATE TABLE IF NOT EXISTS bills (
+    ${columnNames.bill_id} SERIAL UNIQUE,
+    ${columnNames.government_id} INTEGER NOT NULL,
+    ${columnNames.name} TEXT,
     ${columnNames.description} TEXT,
     ${columnNames.closing_date} TEXT,
+    ${columnNames.bill_status} TEXT DEFAULT 'In Progress',
     FOREIGN KEY (${columnNames.government_id}) REFERENCES governments(${columnNames.government_id})
   )`);
-};
+}
 
+async function createResourcesTable() {
+  await pool.query(`CREATE TABLE IF NOT EXISTS resources (
+    ${columnNames.resource_id} SERIAL UNIQUE,
+    ${columnNames.government_id} INTEGER NOT NULL,
+    ${columnNames.name} TEXT,
+    ${columnNames.url} TEXT,
+    ${columnNames.description} TEXT,
+    FOREIGN KEY (${columnNames.government_id}) REFERENCES governments(${columnNames.government_id})
+  )`);
+}
 
 // Helper wrapper functions that return promises that resolve when sql queries are complete.
 
 function run(sqlQuery) {
+  console.log(sqlQuery);
   return new Promise((resolve, reject) => {
-    sqlDb.run(sqlQuery, (err) => {
-      if (err !== null) {
+    pool.query(sqlQuery, (err) => {
+      if (err !== undefined) {
         reject(err);
       } else {
         resolve();
       }
-    })
+    });
   });
-};
+}
 
 function get(sqlQuery) {
   return new Promise((resolve, reject) => {
-    sqlDb.get(sqlQuery, (err, row) => {
-      if (err !== null) {
+    pool.query(sqlQuery, (err, result) => {
+      if (err !== undefined) {
         reject(err);
       } else {
-        resolve(row);
+        const results = { results: result ? result.rows[0] : null };
+        resolve(results.results);
       }
-    })
+    });
   });
-};
+}
 
 function all(sqlQuery) {
   return new Promise((resolve, reject) => {
-    sqlDb.all(sqlQuery, (err, rows) => {
-      if (err !== null) {
+    pool.query(sqlQuery, (err, result) => {
+      if (err !== undefined) {
         reject(err);
       } else {
-        resolve(rows);
+        const results = { results: result ? result.rows : null };
+        resolve(results.results);
       }
-    })
+    });
   });
-};
+}
 
 createDb();
 
--- models/Users.js
+++ models/Users.js
@@ -1,18 +1,18 @@
-const db = require('../db/db_config');
+const db = require("../db/db_config");
 
 /**
  * @typeof User
- * 
- * @prop {number} user_id 
- * @prop {string} username 
- * @prop {string} password 
+ *
+ * @prop {number} user_id
+ * @prop {string} username
+ * @prop {string} password
  * @prop {number} district_id - users district
  * @prop {bool} is_rep - if they are rep for their district
  */
 
 /**
  * @class Users
- * 
+ *
  * Stores all Users.
  * Note that all methods are static.
  * Wherever you import this class, you will be accessing the same data.
@@ -20,16 +20,19 @@ const db = require('../db/db_config');
 class Users {
   /**
    * Add a User.
-   * 
+   *
    * @param {string} username - User name
    * @param {string} password - User password
    * @return {User} - created user
    */
   static async addOne(username, password) {
     // first insert the user into the db then fetch the user from the DB
-    return db.run(`INSERT INTO users (${db.columnNames.username},
-                                     ${db.columnNames.password}) VALUES ('${username}', '${password}')`)
-              .then(() => Users.findByUsername(username));
+    return db
+      .run(
+        `INSERT INTO users (${db.columnNames.username},
+                                     ${db.columnNames.password}) VALUES ('${username}', '${password}')`
+      )
+      .then(() => Users.findByUsername(username));
   }
 
   /**
@@ -38,16 +41,20 @@ class Users {
    * @return {User | undefined} - found User
    */
   static async findByUsername(username) {
-    return db.get(`SELECT * FROM users WHERE ${db.columnNames.username} = '${username}'`);
+    return db.get(
+      `SELECT * FROM users WHERE ${db.columnNames.username} = '${username}'`
+    );
   }
 
   /**
    * Find a User by id.
-   * @param {string} id 
+   * @param {string} id
    * @return {User | undefined} - found User
    */
   static async findByID(id) {
-    return db.get(`SELECT * FROM users WHERE ${db.columnNames.user_id} = '${id}'`);
+    return db.get(
+      `SELECT * FROM users WHERE ${db.columnNames.user_id} = '${id}'`
+    );
   }
 
   /**
@@ -65,12 +72,17 @@ class Users {
    * @return {User | undefined} - updated User
    */
   static async updateUsername(username, new_username) {
-    return db.run(`UPDATE users 
-        SET ${db.columnNames.userName} = '${new_username}' 
-        WHERE ${db.columnNames.userName} = '${username}'`)
-        .then( () => {
-          return Users.findByUsername(new_username);
-        });
+    console.log("updating username...");
+
+    return db
+      .run(
+        `UPDATE users 
+        SET ${db.columnNames.username} = '${new_username}' 
+        WHERE ${db.columnNames.username} = '${username}'`
+      )
+      .then(() => {
+        return Users.findByUsername(new_username);
+      });
   }
   /**
    * Update a Users password
@@ -79,44 +91,42 @@ class Users {
    * @return {User | undefined} - updated User
    */
   static async updatePassword(username, password) {
-    return db.run(`UPDATE users 
+    return db
+      .run(
+        `UPDATE users 
         SET ${db.columnNames.password} = '${password}' 
-        WHERE ${db.columnNames.userName} = '${username}'`)
-        .then( () => {
-          return Users.findByUsername(username);
-        });
+        WHERE ${db.columnNames.username} = '${username}'`
+      )
+      .then(() => {
+        return Users.findByUsername(username);
+      });
   }
-
 
   /**
    * Like post
-   * @param {string} username 
+   * @param {string} username
    * @return {User | undefined} - updated User
    */
   static async likePost(username, post_id) {
-    return Users.findByUsername(username)
-          .then( user => {
-            db.run(`INSERT INTO likes 
+    return Users.findByUsername(username).then((user) => {
+      db.run(`INSERT INTO likes 
               (${db.columnNames.note_id},${db.columnNames.post_id}) 
-              VALUES ('${user.user_id}','${post_id}')`)
-          })
+              VALUES ('${user.user_id}','${post_id}')`);
+    });
   }
 
   /**
    * Disike post
-   * @param {string} username 
+   * @param {string} username
    * @return {User | undefined} - updated User
    */
   static async dislikePost(username, post_id) {
-    return Users.findByUsername(username)
-          .then( user => {
-            db.run(`INSERT INTO dislikes 
+    return Users.findByUsername(username).then((user) => {
+      db.run(`INSERT INTO dislikes 
               (${db.columnNames.note_id},${db.columnNames.post_id}) 
-              VALUES ('${user.user_id}','${post_id}')`)
-          })
+              VALUES ('${user.user_id}','${post_id}')`);
+    });
   }
 }
-
-
 
 module.exports = Users;
